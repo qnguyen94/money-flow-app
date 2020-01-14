c2f724d700e4f0a43f191aaff5d128b59a07defd
Quang Nguyen <vincentnguyen1312@gmail.com>
2020-01-14 15:33:31 -0800

    Updates

---
+++ precache-manifest.7e77d91bcb5f74c424c82967b1edf3ce.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "eb7e419d1bc1a56fa8aac8bae63fb636",
+    "url": "/index.html"
+  },
+  {
+    "revision": "d743a01f18e5cb8587ea",
+    "url": "/static/css/main.5ce5414e.chunk.css"
+  },
+  {
+    "revision": "62b8fe344e111be20c1e",
+    "url": "/static/js/2.cc825be7.chunk.js"
+  },
+  {
+    "revision": "e928fe768baa9832b5bc57eae021f30c",
+    "url": "/static/js/2.cc825be7.chunk.js.LICENSE"
+  },
+  {
+    "revision": "d743a01f18e5cb8587ea",
+    "url": "/static/js/main.8716eec7.chunk.js"
+  },
+  {
+    "revision": "0ffd061d2aa3bdcea684",
+    "url": "/static/js/runtime-main.a0346f93.js"
+  }
+]);
